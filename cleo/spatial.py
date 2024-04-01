44213163b9dd8c81a606b3f44982697d5edf51a9
sebastian <seb.wehrle@gmail.com>
2024-04-01 10:39:54 +0000

    adjust reproject and clip methods

--- cleo/spatial.py
+++ cleo/spatial.py
@@ -2,6 +2,7 @@
 import logging
 import numpy as np
 import geopandas as gpd
+import rasterio.crs
 import xarray as xr
 
 from typing import Union
@@ -20,17 +21,20 @@ def reproject(self, new_crs: str) -> None:
             dst_crs=new_crs,
             nodata=np.nan
         )
+    if self.crs == rasterio.crs.CRS.from_string(new_crs):
+        logging.info(f"Data already in projected in CRS '{new_crs}'")
+    else:
+        try:
+            # reproject each data variable in the dataset
+            data_reproj = self.data.map(reproject_var, keep_attrs=True)
+            logging.info(f"Reprojection of data variables to crs {new_crs} completed")
+            self.data = data_reproj
+            self.crs = rasterio.crs.CRS.from_string(new_crs)
+        except Exception as e:
+            logging.error(f"Error during reprojecting atlas: {e}")
 
-    try:
-        # reproject each data variable in the dataset
-        data_reproj = self.data.map(reproject_var, keep_attrs=True)
-        logging.info(f"Reprojection of data variables to crs {new_crs} completed")
-        self.data = data_reproj
-    except Exception as e:
-        logging.error(f"Error during reprojecting atlas: {e}")
 
-
-def clip_to_geometry(self, clip_shape: Union[str, gpd.GeoDataFrame]) -> xr.Dataset:
+def clip_to_geometry(self, clip_shape: Union[str, gpd.GeoDataFrame]) -> (xr.Dataset, gpd.GeoDataFrame):
     """
     Clip the atlas data to the provided geopandas shapefile
     :param clip_shape_path: Path to the geopandas shapefile representing the clipping area
@@ -55,13 +59,12 @@ def clip_to_geometry(self, clip_shape: Union[str, gpd.GeoDataFrame]) -> xr.Datas
     # Ensure data is loaded and get atlas CRS
     if self.data is None:
         self._build_netcdf()
-    atlas_crs = self.data.rio.crs
 
     # Reproject clip_shape if CRS mismatch
-    if atlas_crs != clip_shape.crs:
-        logging.info(f"Reprojecting clip_shape to {atlas_crs}")
+    if self.crs != clip_shape.crs:
+        logging.info(f"Reprojecting clip_shape to {self.crs}")
         try:
-            clip_shape = clip_shape.to_crs(atlas_crs)
+            clip_shape = clip_shape.to_crs(self.crs)
         except Exception as e:
             logging.error(f"Error reprojecting clip_shape: {e}")
 
@@ -75,4 +78,17 @@ def clip_to_geometry(self, clip_shape: Union[str, gpd.GeoDataFrame]) -> xr.Datas
             continue
     # clipped_data = self.data.rio.clip(clip_shape, all_touched=True)
     logging.info(f"Data clipped")
-    return data_clipped
+    return data_clipped, clip_shape
+
+
+def bbox(self):
+    """
+    Get the bounding box of the Atlas object
+    :param self: an Atlas object
+    :return: (xmin, ymin, xmax, ymax)
+    """
+    return (self.data.coords["x"].min(),
+            self.data.coords["y"].min(),
+            self.data.coords["x"].max(),
+            self.data.coords["y"].max(),
+            )
